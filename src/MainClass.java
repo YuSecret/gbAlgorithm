8a0e6aa5041e7c977af5ce8d92190563016ac9f6
YuSecret <4sovremennik@gmail.com>
2019-12-22 21:04:54 +0300

    homeWork2

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,7 +4,27 @@ import java.util.Scanner;
 public class MainClass {
     static Random rand = new Random();
     public static void main(String[] args) {
-        
+        //2 .Создать программу, которая переворачивает вводимые строки (читает справа налево).
+        //System.out.println(" Результат: " + ReadRToL("TExt"));
+        //3. Создать класс для реализации дека.
+
+        MyDec md = new MyDec(5);
+        md.insertLeft(4);
+        md.insertRight(1);
+
+        md.insertRight(2);
+
+        md.insertLeft(3);
+        System.out.println(md.toString());
+
+        System.out.println(md.removeLeft());
+    }
+    public static String ReadRToL(String s) {
+        String result = "";
+        for (int i = s.length()-1 ; i>=0 ; i--) {
+            result+=s.charAt(i);
+        }
+        return result;
     }
     public static void homeWork1(String[] args) {
         FirstArrayList <Integer> arr = new FirstArrayList<>(100000);
---
+++ src/MyDec.java
@@ -0,0 +1,93 @@
+import java.util.EmptyStackException;
+
+public class MyDec<T> {
+
+    private T[] list;
+    private int size = 0;
+    private final int DEFAULT_CAPACITY = 10;
+    private int begin = 0;
+    private int end = 0;
+
+
+    public MyDec(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("capacity: " + capacity);
+        }
+        list = (T[]) new Object[capacity];
+    }
+
+    public MyDec() {
+        list = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public void insertRight(T value) {
+        if (isFull()) {
+            throw new StackOverflowError();
+        }
+        list[end] = value;
+        size++;
+        end = nextIndex(end);
+
+    }
+
+    public void insertLeft(T value) {
+        if (isFull()) {
+            throw new StackOverflowError();
+        }
+       size++;
+       System.arraycopy(list,0,list,1,size-1);
+       list[begin] = value;
+
+       end = nextIndex(end);
+
+    }
+    public T removeLeft() {
+        T temp = peekLeft();
+        size--;
+        list[begin] = null;
+        begin = nextIndex(begin);
+        return temp;
+    }
+    public T removeRight() {
+        T temp = peekRight();
+        size--;
+        list[end] = null;
+        end = nextIndex(end);
+        return temp;
+    }
+    public T peekLeft() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return list[begin];
+    }
+    public T peekRight() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return list[end];
+    }
+    private int nextIndex(int index) {
+        return (index + 1) % list.length;
+    }
+
+    public boolean isFull() {
+        return size == list.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+    public String toString() {
+        String result = "[";
+        for (int i = 0 ; i <size; i++) {
+            result += " "+list[i];
+        }
+        result += " ]";
+        return result;
+    }
+}
