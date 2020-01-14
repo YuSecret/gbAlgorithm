1a0b41f59428cdd04daed74d35a1f65d4ade2c7a
YuSecret <4sovremennik@gmail.com>
2020-01-14 21:31:47 +0300

    homeWork4

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,16 +2,6 @@ import java.util.Iterator;
 
 public class MainClass {
     public static void main(String[] args) {
-        LinkedStack<String> ls = new LinkedStack<>();
-        ls.push("1");
-        ls.push("2");
-        ls.push("3");
-        ls.push("4");
 
-        System.out.println(ls.toString());
-        ls.pop("1");
-        System.out.println(ls.toString());
-
-        System.out.println(ls.peek());
     }
 }
